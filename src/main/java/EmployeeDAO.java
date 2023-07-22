e6a33caf68a213b20c51b27c9598cada72691aad
Егор <bountyfoolmoon89@yandex.com>
2023-07-22 23:58:45 +0300

    1

---
+++ src/main/java/EmployeeDAO.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface EmployeeDAO {
+    void createEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    List<Employee> getAllEmployees();
+    void updateEmployee(Employee employee);
+    void deleteEmployeeById(long id);
+}
