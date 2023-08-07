c0871fa604962527041d4e436aa26ed282a58cd8
Егор <bountyfoolmoon89@yandex.com>
2023-08-07 21:38:21 +0300

    3fix

--- src/main/java/HibernateSessionFactoryUtil.java
+++ src/main/java/HibernateSessionFactoryUtil.java
@@ -1,4 +1,5 @@
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
@@ -7,18 +8,19 @@ public class HibernateSessionFactoryUtil {
 
     private HibernateSessionFactoryUtil() {}
 
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration().configure();
-                configuration.addAnnotatedClass(Employee.class);
-                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-                sessionFactory = configuration.buildSessionFactory(builder.build());
-
-            } catch (Exception e) {
-                System.out.println("Исключение!" + e);
-            }
+    static {
+        try {
+            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            sessionFactory = configuration.buildSessionFactory(registry);
+        } catch (Exception e) {
+            System.out.println("Исключение!" + e);
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 }
